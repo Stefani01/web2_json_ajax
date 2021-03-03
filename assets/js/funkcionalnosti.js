f21a6d1ae6ebc43cc25f44b922630241d5df5133
StefaniSimic <59971963+StefaniSimic@users.noreply.github.com>
2021-03-03 20:15:41 +0100

    03.03.2021-2

--- assets/js/funkcionalnosti.js
+++ assets/js/funkcionalnosti.js
@@ -3,6 +3,25 @@ $(document).ready(function(){
     $("#btnMeni").click(function(){
         $("#meniToggle").slideToggle(1000);
     })
-
+    
+    var nizElKojimaSeDodeljujeAnimacijaSlideToggle = Array("#test", "#slanje", "#kupovina", "#ostecenje", "#proizvod");
+    dodeliAnimacijuSlideToggle(nizElKojimaSeDodeljujeAnimacijaSlideToggle);
+   
     
 })
+
+function dodeliAnimacijuSlideToggle(niz){
+    niz.forEach(element => {
+        elementSLideToggle(element);
+    });
+}
+
+function elementSLideToggle(elementClick){
+    $(elementClick).click(function(){
+        var element = $(this);
+        element.next().slideToggle('slow');
+        element.find(".fa-chevron-down").toggle();
+        element.find(".fa-chevron-up").toggle();
+    })
+}
+
